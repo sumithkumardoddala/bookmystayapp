9a36c04c5d792f9c5814e0577906dd9ab833e31d
sumithkumardddala <sumithkumardoddala@gmail.com>
2026-03-17 09:29:15 +0530

    uc1 created

---
+++ BookMyStayApp/src/Application.java
@@ -0,0 +1,18 @@
+/**
+ * HotelBookingApp
+ * Entry point for the Hotel Booking System application.
+ * Displays a welcome message when the application starts.
+ *
+ * @author ni
+ * @version 1.0
+ */
+
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Hotel Booking System v1.0");
+        System.out.println("==================================");
+        System.out.println("System Initialized successfully.");
+
+    }
+}
